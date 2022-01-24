e2a3768fe1dbafa021c4c3ff9e80978b8e435b52
anonymousdepu <depu143jsr@gmail.com>
2022-01-24 22:38:03 +0530

    Updates

---
+++ precache-manifest.eddaf34aad6f77d19389aebc90a02383.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d168efa3b978d41b97c2706ce58f1383",
+    "url": "/canteen/index.html"
+  },
+  {
+    "revision": "35e13502222af7374c45",
+    "url": "/canteen/static/css/main.7136409c.chunk.css"
+  },
+  {
+    "revision": "b6412378828b2778dc17",
+    "url": "/canteen/static/js/2.61f38fbf.chunk.js"
+  },
+  {
+    "revision": "35e13502222af7374c45",
+    "url": "/canteen/static/js/main.89c3e372.chunk.js"
+  },
+  {
+    "revision": "eb6a88c91f4c1b5f954e",
+    "url": "/canteen/static/js/runtime~main.583bb698.js"
+  }
+]);
